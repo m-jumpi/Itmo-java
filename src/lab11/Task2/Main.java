a77e85c62b047acc84a63d4cfac91fd477bb526b
denis <jumpibox@gmail.com>
2022-06-09 19:17:12 +0400

    lab10

--- src/lab11/Task2/Main.java
+++ src/lab11/Task2/Main.java
@@ -1,11 +1,17 @@
 package lab11.Task2;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread myThread = new MyThread();
+
         Thread.State state = myThread.getState();
         System.out.println(state);
+
         myThread.start();
+        state = myThread.getState();
+        System.out.println(state);
+
+        myThread.join();
         state = myThread.getState();
         System.out.println(state);
     }
--- src/lab11/Task2/MyThread.java
+++ src/lab11/Task2/MyThread.java
@@ -3,8 +3,6 @@ package lab11.Task2;
 class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println("hello");
-        Thread.State state = currentThread().getState();
-        System.out.println(state);
+        System.out.println("hello world");
     }
 }
